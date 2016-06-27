929064f6a348c8323d37209f259add945afde0a3
Sijawusz Pur Rahnama <sija@sija.pl>
2016-06-27 19:35:38 +0200

    Add spec for `GPhoto2.result_as_string`

--- spec/gphoto2_spec.cr
+++ spec/gphoto2_spec.cr
@@ -1,6 +1,19 @@
 require "./spec_helper"
 
 describe GPhoto2 do
+  describe ".result_as_string" do
+    it "converts return code to proper string" do
+      codez = {
+        -107 => "Directory not found",
+        -37  => "Error updating the port settings",
+        -7   => "I/O problem"
+      }
+      codez.each do |code, string|
+        GPhoto2.result_as_string(code).should eq(string)
+      end
+    end
+  end
+
   describe ".check!" do
     context "the return code is not GP_OK" do
       it "raises GPhoto2::Error with a message and error code" do
