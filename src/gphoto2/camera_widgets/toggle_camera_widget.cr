2562174d55163d00411412406261a49c48e1a93c
Sijawusz Pur Rahnama <sija@sija.pl>
2016-07-06 13:41:32 +0200

    ToggleCameraWidget#value is Int32 not Bool
    Might be of value `2` for instance

--- src/gphoto2/camera_widgets/toggle_camera_widget.cr
+++ src/gphoto2/camera_widgets/toggle_camera_widget.cr
@@ -2,13 +2,13 @@ require "../camera_widget"
 
 module GPhoto2
   class ToggleCameraWidget < CameraWidget
-    protected def get_value : Bool
+    protected def get_value : Int32
       ptr = Pointer(LibC::Int).malloc
       GPhoto2.check! FFI::LibGPhoto2.gp_widget_get_value(self, ptr)
-      ptr.value == 1
+      ptr.value
     end
 
-    protected def set_value(value : Bool)
+    protected def set_value(value : Int32)
       GPhoto2.check! FFI::LibGPhoto2.gp_widget_set_value(self, pointerof(value))
     end
   end
