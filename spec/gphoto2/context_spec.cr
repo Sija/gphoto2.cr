4b65faf74788018128190e8bca479b74588032a1
Sijawusz Pur Rahnama <sija@sija.pl>
2016-07-06 02:25:42 +0200

    Fix Context spec

--- spec/gphoto2/context_spec.cr
+++ spec/gphoto2/context_spec.cr
@@ -5,9 +5,9 @@ module GPhoto2
     describe "#finalize" do
       it "decrements the reference counter" do
         context = Context.new
-        context.ptr.should be_a Pointer(FFI::LibGPhoto2::GPContext)
+        context.@ptr.should be_a Pointer(FFI::LibGPhoto2::GPContext)
         context.finalize
-        context.ptr.should be_nil
+        context.@ptr.should be_nil
       end
     end
   end
