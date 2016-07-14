afacdf427567e407db4118cf752dbb07f646e6ef
Sijawusz Pur Rahnama <sija@sija.pl>
2016-07-14 04:19:47 +0200

    Split CameraFileInfo
    - use ManagedStruct, since we need to copy the pointer value
    - misc. fixes

--- src/gphoto2/camera_file_info.cr
+++ src/gphoto2/camera_file_info.cr
@@ -2,52 +2,26 @@ require "./struct"
 
 module GPhoto2
   abstract class CameraFileInfo
-    # include GPhoto2::Struct(LibGPhoto2::CameraFileInfo)
+    # include GPhoto2::ManagedStruct(LibGPhoto2::CameraFileInfo)
 
     def fields
       wrapped.fields
     end
 
-    def has_field?(field : Symbol | String)
+    def has_field?(field : String | Symbol)
       fields.includes? LibGPhoto2::CameraFileInfoFields.parse(field.to_s)
     end
 
-    def status : LibGPhoto2::CameraFileStatus
-      wrapped.status
+    def status : LibGPhoto2::CameraFileStatus?
+      wrapped.status if has_field?(:status)
     end
 
-    def size : UInt64
-      wrapped.size
+    def size : LibC::UInt64T?
+      wrapped.size if has_field?(:size)
     end
 
     def type : String?
-      if wrapped.type.size > 0
-        String.new wrapped.type.to_unsafe
-      end
-    end
-  end
-
-  class CameraFileInfoFile < CameraFileInfo
-    include GPhoto2::Struct(LibGPhoto2::CameraFileInfoFile)
-
-    def readable? : Bool
-      wrapped.permissions.read?
-    end
-
-    def deletable? : Bool
-      wrapped.permissions.delete?
-    end
-
-    def mtime : Time
-      Time.epoch(wrapped.mtime)
-    end
-
-    def width : UInt32?
-      wrapped.width if wrapped.width > 0
-    end
-
-    def height : UInt32?
-      wrapped.height if wrapped.height > 0
+      String.new wrapped.type.to_unsafe if has_field?(:type)
     end
   end
 end
---
+++ src/gphoto2/camera_file_info_file.cr
@@ -0,0 +1,27 @@
+require "./struct"
+
+module GPhoto2
+  class CameraFileInfoFile < CameraFileInfo
+    include GPhoto2::ManagedStruct(LibGPhoto2::CameraFileInfoFile)
+
+    def readable? : Bool
+      has_field?(:permissions) && wrapped.permissions.read?
+    end
+
+    def deletable? : Bool
+      has_field?(:permissions) && wrapped.permissions.delete?
+    end
+
+    def mtime : Time?
+      Time.epoch(wrapped.mtime) if has_field?(:mtime)
+    end
+
+    def width : LibC::UInt32T?
+      wrapped.width if has_field?(:width)
+    end
+
+    def height : LibC::UInt32T?
+      wrapped.height if has_field?(:height)
+    end
+  end
+end
