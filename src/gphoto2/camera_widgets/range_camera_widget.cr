f88a663834d269f6e88f1c51079b583d1fb589b9
Sijawusz Pur Rahnama <sija@sija.pl>
2016-07-20 23:54:14 +0200

    Fix RangeCameraWidget implementation
    Refs #4

--- src/gphoto2/camera_widgets/range_camera_widget.cr
+++ src/gphoto2/camera_widgets/range_camera_widget.cr
@@ -2,16 +2,24 @@ require "../camera_widget"
 
 module GPhoto2
   class RangeCameraWidget < CameraWidget
-    # FIXME: Float => Int, hmmm...
     def range
+      # TODO: remove it once Range start accepting float as type
+      # min, max, inc = get_range
+      # (min..max).step(inc).to_a
       min, max, inc = get_range
-      (min.to_i..max.to_i).step(inc.to_i).to_a
+      arr = [] of Float32
+      i = min
+      while i <= max
+        arr << i.to_f32
+        i += inc
+      end
+      arr
     end
 
     private def get_range
-      min, max, inc = 0_f32, 0_f32, 0_f32
+      min, max, inc = LibC::Float[3]
       GPhoto2.check! LibGPhoto2.gp_widget_get_range(self, pointerof(min), pointerof(max), pointerof(inc))
-      [min, max, inc]
+      {min, max, inc}
     end
 
     protected def get_value
@@ -23,7 +31,7 @@ module GPhoto2
     protected def set_value(value)
       case value
       when Float32
-        ptr = Pointer(LibC::Float).malloc 1, value
+        ptr = Pointer(LibC::Float).malloc 1, value.to_f32
         set_value_ptr ptr
       end
     end
