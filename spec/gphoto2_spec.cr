c4a87902fa2aee9ad6e2ed68cb17bee290c72248
Sijawusz Pur Rahnama <sija@sija.pl>
2016-08-25 22:56:15 +0200

    Add specs for GPhoto2.debug?

--- spec/gphoto2_spec.cr
+++ spec/gphoto2_spec.cr
@@ -1,6 +1,7 @@
 require "./spec_helper"
 
 VERSION_PATTERN = /^\d+(\.\d+){2,3}(-\w+)?$/
+ENV_DEBUG_KEY = "DEBUG"
 
 describe GPhoto2 do
   describe "VERSION" do
@@ -68,12 +69,49 @@ describe GPhoto2 do
         code = LibGPhoto2::GP_ERROR
         message = "Unspecified error (#{code})"
 
-        expect_raises(GPhoto2::Error, message) { GPhoto2.check!(code) }
+        expect_raises(GPhoto2::Error, message) do
+          GPhoto2.check!(code)
+        end
       end
     end
     context "the return code is a value" do
       it "returns back the value" do
         GPhoto2.check!(10).should eq 10
+      end
+    end
+  end
+
+  describe ".debug?" do
+    context "ENV debug key is set to '1'" do
+      debug_flag = ENV[ENV_DEBUG_KEY]?
+      begin
+        ENV[ENV_DEBUG_KEY] = "1"
+        it "should be true" do
+          GPhoto2.debug?.should be_true
+        end
+      ensure
+        ENV[ENV_DEBUG_KEY] = debug_flag
+      end
+    end
+    context "ENV debug key is set to value other than '1'" do
+      debug_flag = ENV[ENV_DEBUG_KEY]?
+      begin
+        ENV[ENV_DEBUG_KEY] = "10"
+        it "should be false" do
+          GPhoto2.debug?.should be_false
+        end
+      ensure
+        ENV[ENV_DEBUG_KEY] = debug_flag
+      end
+    end
+    context "ENV debug key is unset" do
+      debug_flag = ENV.delete ENV_DEBUG_KEY
+      begin
+        it "should be false" do
+          GPhoto2.debug?.should be_false
+        end
+      ensure
+        ENV[ENV_DEBUG_KEY] = debug_flag
       end
     end
   end
