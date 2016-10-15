344099df239fa17f8d4fffd2b3addc046e52456e
Sijawusz Pur Rahnama <sija@sija.pl>
2016-10-15 22:35:58 +0200

    Add camera port reset example

---
+++ examples/reset_camera_port.cr
@@ -0,0 +1,14 @@
+require "../src/gphoto2"
+
+# Reset port on first found camera
+
+GPhoto2::Camera.first do |camera|
+  puts "Resetting %s on port %s" % [
+    camera.model.colorize(:green),
+    camera.port.colorize(:blue)
+  ]
+  camera.with_port do |port|
+    camera.exit
+    port.reset
+  end
+end
