cbbd19371dc478a2c4692012162294c8f40298b3
Sijawusz Pur Rahnama <sija@sija.pl>
2018-09-21 03:02:57 +0200

    Fix nillable argument type

--- src/gphoto2/camera/configuration.cr
+++ src/gphoto2/camera/configuration.cr
@@ -38,7 +38,7 @@ module GPhoto2
       # camera[:aperture] # => 4
       # camera[:iso]      # => 400
       # ```
-      def preserving_config(keys : Array(String | Symbol) = nil) : Void
+      def preserving_config(keys : Array(String | Symbol)? = nil) : Void
         config_snapshot = keys ? config.select(keys.map &.to_s) : config
         config_snapshot = config_snapshot.reduce({} of String => String) do |memo, (key, widget)|
           memo[key] = widget.to_s rescue NotImplementedError
