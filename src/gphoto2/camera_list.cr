ac7ee63f8732a69c16d33c1ca1441678180a59af
Sijawusz Pur Rahnama <sija@sija.pl>
2023-12-26 00:53:20 +0100

    Refactor `CameraList` and remove obsolete `Entry`

--- src/gphoto2/camera_list.cr
+++ src/gphoto2/camera_list.cr
@@ -4,6 +4,10 @@ module GPhoto2
   class CameraList
     include GPhoto2::Struct(LibGPhoto2::CameraList)
 
+    record Entry,
+      name : String,
+      value : String
+
     # NOTE: allocates memory.
     def initialize
       new
@@ -21,7 +25,9 @@ module GPhoto2
 
     # Returns an array of entries in the list.
     def to_a : Array(Entry)
-      Array(Entry).new(size) { |i| Entry.new(self, i) }
+      Array(Entry).new(size) do |i|
+        Entry.new get_name(i), get_value(i)
+      end
     end
 
     private def new
@@ -39,6 +45,18 @@ module GPhoto2
     private def count
       GPhoto2.check! \
         LibGPhoto2.gp_list_count(self)
+    end
+
+    private def get_name(idx)
+      GPhoto2.check! \
+        LibGPhoto2.gp_list_get_name(self, idx, out ptr)
+      ptr ? String.new(ptr) : ""
+    end
+
+    private def get_value(idx)
+      GPhoto2.check! \
+        LibGPhoto2.gp_list_get_value(self, idx, out ptr)
+      ptr ? String.new(ptr) : ""
     end
   end
 end
