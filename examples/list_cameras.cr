48496cc590e097c5679665f72a12ec9442087a59
Sijawusz Pur Rahnama <sija@sija.pl>
2024-07-23 14:11:37 +0200

    Add `Camera::ID#serialnumber` QoL method

--- examples/list_cameras.cr
+++ examples/list_cameras.cr
@@ -9,6 +9,7 @@ cameras.each do |camera|
   puts camera.model.colorize(:green)
   props = {
     "id":                camera.id,
+    "serial number":     camera.serialnumber,
     "port":              camera.port,
     "driver status":     abilities.status,
     "device type":       abilities.device_type,
--- src/gphoto2/camera/id.cr
+++ src/gphoto2/camera/id.cr
@@ -5,13 +5,16 @@ module GPhoto2
     module ID
       UUID_NS = UUID.new("cc4daadf-37f5-4a30-8f1b-33a49a08e012")
 
+      # Returns the serial number of the camera if present.
+      def serialnumber : String?
+        self[:serialnumber]?.try(&.value?).try(&.to_s)
+      end
+
       # Returns the ID of the camera.
       #
       # WARNING: this ID is not guaranteed to be unique.
       getter id : UUID do
-        sn = self[:serialnumber]?.try(&.value?)
-        id = "#{model}-#{sn || port}"
-
+        id = "#{model}-#{serialnumber || port}"
         UUID.v5(id, UUID_NS)
       end
     end
